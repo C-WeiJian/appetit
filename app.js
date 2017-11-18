ec9fe2e43ab9e764c2135833b147f9cce275746b
WeiJian <chen_wei_jian5566@hotmail.com>
2017-11-18 15:26:42 +0000

    luis integration

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
-// Get secrets from server environment
-var botConnectorOptions = { 
-    // appId: process.env.BOTFRAMEWORK_APPID, 
-    // appPassword: process.env.BOTFRAMEWORK_APPSECRET
-
-    appId: '4c370df0-faeb-408b-b535-52ad282273a0',
-    appPassword: 'rsaizSV7828*rxGVZEJ3@%='
-};
-
-// Create bot
-var connector = new builder.ChatConnector(botConnectorOptions);
-var bot = new builder.UniversalBot(connector);
-
-bot.dialog('/', function (session) {
-    
-    //respond with user's message
-    session.send("You said " + session.message.text);
-});
-
 // Setup Restify Server
 var server = restify.createServer();
 
@@ -44,3 +25,53 @@ server.get('/', restify.plugins.serveStatic({
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
+
+
+// Get secrets from server environment
+var botConnectorOptions = { 
+    // appId: process.env.BOTFRAMEWORK_APPID, 
+    // appPassword: process.env.BOTFRAMEWORK_APPSECRET
+
+    appId: '4c370df0-faeb-408b-b535-52ad282273a0',
+    appPassword: 'rsaizSV7828*rxGVZEJ3@%='
+};
+
+// Create bot
+var connector = new builder.ChatConnector(botConnectorOptions);
+var bot = new builder.UniversalBot(connector);
+
+const LuisModelUrl = '	https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/9646960e-306f-4203-865f-5cb70c538658?subscription-key=3f76e6d2a6b54d3aa8957b995143bad7&timezoneOffset=0';
+var recogniser = new builder.LuisRecognizer(LuisModelUrl);
+
+var intents = new builder.IntentDialog({recognizers:[recogniser]});
+//intents.matches(/\b(hi|hello|hey|sup)\b/i,'/sayHi');
+//intents.matches(/\b(yes|yup|okay)\b/i,'/sayYes');
+//intents.matches(/\b(no)\b/i,'/sayNo');
+intents.matches('orderFood', '/orderFood');
+//intents.matches('analyseImage', '/giveImageAnalysis');
+//intents.matches('getFunFact','/funFact');
+//intents.matches('getloc','/getLoc');
+intents.onDefault(builder.DialogAction.send("Sorry, I didn't understand what you said."))
+
+
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+// This is called the root dialog. It is the first point of entry for any message the bot receives
+
+
+// bot.dialog('/', function (session) {
+    
+//     //respond with user's message
+//     session.send("You said " + session.message.text);
+// });
+
+bot.dialog('/', intents);
+
+bot.dialog('/orderFood', [
+    function (session){
+        session.endDialog("Sorry. No food.");
+    }
+]);
